c0589833d0018a52553f743e92a969f712c99e74
yravin <ryravin25@Email.com>
2026-03-14 16:07:59 +0700

    fix: remove .env and db.sqlite3 from tracking

--- farmer_digital/settings.py
+++ farmer_digital/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+import os
 
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
